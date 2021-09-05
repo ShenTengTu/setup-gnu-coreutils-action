f895afc5c8b134f85c70a9c4d52627ad30c97087
Shen-Teng Tu <tusamten@gmail.com>
2021-09-06 00:14:41 +0800

    grouping log lines

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd "$GITHUB_WORKSPACE" || exit $?
+
+echo "::group::Get GNU coreutils repository"
+
 echo "Clone GNU coreutils."
 git clone git://git.sv.gnu.org/coreutils gnu-coreutils
 
@@ -10,10 +13,18 @@ echo "Initialize submodule."
 git submodule init
 git submodule update
 
+echo "::endgroup::"
+
+echo "::group::Setup GNU coreutils (bootstrap)"
+
 echo "Get and check other files needed to build."
-./bootstrap --skip-po --no-git --gnulib-srcdir="$repo/gnulib" >/dev/null || exit $?
+./bootstrap --skip-po --no-git --gnulib-srcdir="$repo/gnulib" || exit $?
+
+echo "::endgroup::"
 
 prefix=$HOME/gnu-coreutils
+
+echo "::group::Setup GNU coreutils (configure)"
 
 echo "Configure the package."
 ./configure CFLAGS="-m64 -Wno-type-limits" LDFLAGs="-m64" \
@@ -24,11 +35,20 @@ echo "Configure the package."
 --disable-nls \
 --quiet
 
+echo "::endgroup::"
+
+echo "::group::Setup GNU coreutils (compile)"
+
 echo "Compile coreutils"
 make -j"$(nproc)"
+
+echo "::endgroup::"
+
+echo "::group::Setup GNU coreutils (install)"
 
 echo "Install coreutils into $prefix"
 make -j"$(nproc)" install
 
 echo "$prefix/bin" >>$GITHUB_PATH
 
+echo "::endgroup::"
